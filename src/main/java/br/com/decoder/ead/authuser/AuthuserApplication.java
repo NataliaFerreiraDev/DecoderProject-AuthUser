da399fcb2b3ceb972a9d1771255204831f264778
Natalia Ferreira <nataliaferreira.dev@yahoo.com.br>
2023-07-31 07:59:10 -0300

    Inclusao do service-registry

--- src/main/java/br/com/decoder/ead/authuser/AuthuserApplication.java
+++ src/main/java/br/com/decoder/ead/authuser/AuthuserApplication.java
@@ -2,8 +2,10 @@ package br.com.decoder.ead.authuser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class AuthuserApplication {
 
 	public static void main(String[] args) {
